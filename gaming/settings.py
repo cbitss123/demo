780c045a3015ec004005046690d1aac6950cfb4d
cbitss123 <88833989+cbitss123@users.noreply.github.com>
2021-08-14 15:38:35 +0530

    Update settings.py

--- gaming/settings.py
+++ gaming/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'f@lw2h88o2#o+@51yxfckl@z*p1%g4mu=b6krsffiqh%sh%wa^'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
